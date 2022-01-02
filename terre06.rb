ff0d3ce5d4f1b59411ce43488cde673f87d509e8
scosy <amine.bentaleb064@gmail.com>
2022-01-02 07:16:44 +0100

    Finished the reverse string exercise.

---
+++ terre06.rb
@@ -0,0 +1,11 @@
+# Créez un programme qui affiche l’inverse de la chaîne de caractères donnée en argument.
+#split, map pour renvoyer l'index - 1
+def reverse_string(string)
+  if string.class == NilClass || ARGV.length > 1
+    p "Veuillez entrer une chaîne de caractères."
+  else
+    p string.split(//).map.with_index { |letter, index| string[string.length - 1 - index] }.join
+  end
+  end
+
+reverse_string(ARGV[0])
