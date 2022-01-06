6602549ebe47fc061c361647636942a693c8083a
scosy <amine.bentaleb064@gmail.com>
2022-01-06 05:42:54 +0100

    Added errors.

--- terre06.rb
+++ terre06.rb
@@ -1,11 +1,17 @@
 # Créez un programme qui affiche l’inverse de la chaîne de caractères donnée en argument.
 
-def reverse_string(string)
-  if string.class == NilClass || ARGV.length > 1
-    puts "Veuillez entrer une chaîne de caractères."
-  else
-    puts string.split(//).map.with_index { |letter, index| string[string.length - 1 - index] }.join
-  end
-  end
+def only_numbers(str)
+  check = true
+  str.each_char { |c| c.match?(/[a-zA-Z]/) ? check = false : () }
+  check
+end
 
-reverse_string(ARGV[0])
+def reverse_string(string)
+  string.split(//).map.with_index { |letter, index| string[string.length - 1 - index] }.join
+end
+
+(puts "error"; exit) if ARGV.size != 1
+(puts "error"; exit) if only_numbers(ARGV[0])
+
+
+puts reverse_string(ARGV[0])
