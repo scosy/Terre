92052db389a23c24a24dd6fb4d318f6acf2e941e
scosy <amine.bentaleb064@gmail.com>
2022-01-06 05:28:40 +0100

    Refactored code.

--- terre04.rb
+++ terre04.rb
@@ -1,13 +1,16 @@
 # Créez un programme qui permet de déterminer si l’argument donné est un entier pair ou impair..
 
-def is_even(number)
-  if number.class == NilClass || number.match(/[a-zA-Z]/)
-    puts 'Tu bluffes, Martoni'
-  elsif number.to_i % 2 == 0
-    puts 'pair'
-  elsif number.to_i % 2 != 0
-    puts 'impair'
-  end
+def is_number(str)
+  str.match?(/[0-9]/)
 end
 
-is_even(ARGV[0])
+def is_even(number)
+  number.to_i % 2 == 0 ? "pair" : "impair"
+end
+
+(puts "error"; exit) if ARGV.size != 1
+(puts "error"; exit) if !is_number(ARGV[0])
+
+number = ARGV[0].to_i
+
+puts is_even(number)
