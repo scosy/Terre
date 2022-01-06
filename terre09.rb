fff061dbbe10b78c17354cbc79aa635d678d34b7
scosy <amine.bentaleb064@gmail.com>
2022-01-06 06:01:08 +0100

    Fixed error cases.

--- terre09.rb
+++ terre09.rb
@@ -1,13 +1,18 @@
 # Créez un programme qui affiche la racine carrée d’un entier positif.
 
+def is_number(str)
+  str.match?(/[0-9]/)
+end
+
 def square_root(number)
-  if number.class == NilClass || number.match?(/[a-zA-Z]/) || ARGV.length > 1
-    puts "erreur."
-  else
     for i in 0..(number.to_i)
       (i * i == number.to_i) ? (puts i; exit) : ()
     end
-  end
 end
+
+
+(puts "error"; exit) if ARGV.size != 1
+(puts "error"; exit) if !is_number(ARGV[0])
+(puts "error"; exit) if ARGV[0] <= "0"
 
 square_root(ARGV[0])
