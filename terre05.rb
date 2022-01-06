9e1041ed1096e4ebb9b9122e50d5abb94324f3ff
scosy <amine.bentaleb064@gmail.com>
2022-01-06 05:33:58 +0100

    Added errors.

--- terre05.rb
+++ terre05.rb
@@ -1,9 +1,18 @@
 #Créez un programme qui affiche le résultat et le reste d’une division entre deux nombres.
 
+def are_number(str1, str2)
+  str1.match?(/[0-9]/) && str2.match?(/[0-9]/)
+end
+
 def divide(n1, n2)
   result = n1 / n2
   rest = n1 % n2
-  puts "résultat: #{result} \nreste: #{rest}"
+  "résultat: #{result} \nreste: #{rest}"
 end
 
-divide(ARGV[0].to_i, ARGV[1].to_i)
+(puts "error"; exit) if ARGV.size != 2
+(puts "error"; exit) if !are_number(ARGV[0], ARGV[1])
+(puts "error"; exit) if ARGV[1] == "0"
+
+
+puts divide(ARGV[0].to_i, ARGV[1].to_i)
