c31765cc5de8021633c8571114a8faa04e99f683
scosy <amine.bentaleb064@gmail.com>
2022-01-06 05:50:40 +0100

    Fixed error cases.

--- terre08.rb
+++ terre08.rb
@@ -1,12 +1,18 @@
 # Créez un programme qui affiche le résultat d’une puissance.
 # L’exposant ne pourra pas être négatif.
 
-def exponential(number, exponent)
-  if number.class == NilClass || number.match?(/[a-zA-Z]/) || ARGV.length > 2 || ARGV[1].match?(/\W/)
-    puts "erreur."
-  else
-    puts number.to_i**exponent.to_i
-  end
+def are_number(str1, str2)
+  str1.match?(/[0-9]/) && str2.match?(/[0-9]/)
 end
 
-exponential(ARGV[0], ARGV[1])
+def exponential(number, exponent)
+  number.to_i**exponent.to_i
+end
+
+
+(puts "error"; exit) if ARGV.size != 2
+(puts "error"; exit) if !are_number(ARGV[0], ARGV[1])
+(puts "error"; exit) if ARGV[1] <= "0"
+
+
+puts exponential(ARGV[0], ARGV[1])
