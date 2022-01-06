e9e0f6d76386f587deaee6d843accd64161ef31c
scosy <amine.bentaleb064@gmail.com>
2022-01-06 06:10:08 +0100

    Fixed error cases.

--- terre10.rb
+++ terre10.rb
@@ -1,17 +1,20 @@
 # Créez un programme qui affiche si un nombre est premier ou pas.
 
-def prime_number(number)
-  if number.class == NilClass || number.match?(/[a-zA-Z]/) || ARGV.length > 1
-    puts "erreur."
-  else
-    if number.to_i == 2
-      puts "Oui, #{number} est un nombre premier."
-    elsif number.to_i == 1 || number.to_i % 2 == 0 || number.to_i % 3 == 0 || number.to_i % 5 == 0
-      puts "Non #{number} n'est pas un nombre premier."
-    else
-      puts "Oui, #{number} est un nombre premier."
-    end
-  end
+def contains_letter(str)
+  str.match?(/[a-zA-Z]/)
 end
 
-prime_number(ARGV[0])
+def prime_number(number)
+  prime= ["2", "3", "5"]
+    if (!prime.include?(number)) && (number.to_i == 1 || number.to_i % 2 == 0 || number.to_i % 3 == 0 || number.to_i % 5 == 0 )
+      "Non #{number} n'est pas un nombre premier."
+    else
+      "Oui, #{number} est un nombre premier."
+    end
+end
+
+(puts "error"; exit) if ARGV.size != 1
+(puts "error"; exit) if contains_letter(ARGV[0])
+(puts "error"; exit) if ARGV[0] <= "0"
+
+puts prime_number(ARGV[0])
