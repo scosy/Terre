959c80e5dd13024429c9ca100167ad018170dea6
scosy <amine.bentaleb064@gmail.com>
2022-01-06 05:19:54 +0100

    Rewrited code to fix display and optimise clarity.

--- terre03.rb
+++ terre03.rb
@@ -1,16 +1,18 @@
 # Créez un programme qui affiche l’alphabet à partir de la lettre donnée
 # en argument en lettres minuscules suivi d’un retour à la ligne.
 
-def alphabet_from_n(n)
-  if n.match(/[a-zA-Z]/)
-    for letter in n.."z"
-      puts letter
-    end
-  else
-    puts "Veuillez entrer une lettre svp."
-  end
+def letter_only(str)
+  str.match(/[a-zA-Z]/)
 end
 
-alphabet_from_n(ARGV[0])
+def alphabet_from_n(n)
+    alphabet = *(n.."z") 
+    alphabet.join(" ")
+end
+
+(puts "error"; exit) if ARGV.size != 1 || ARGV[0].size != 1
+(puts "error"; exit) if !letter_only(ARGV[0])
+
+puts alphabet_from_n(ARGV[0])
 
 # Reformuler code pour séparer fonctions, gestion d'erreur, parsing, résolution et afffichage du résultat.
