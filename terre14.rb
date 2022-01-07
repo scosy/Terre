5848bce8f8bdd1f3c8b210da91581f07b13f5648
scosy <amine.bentaleb064@gmail.com>
2022-01-07 05:59:53 +0100

    Refactored code and added error cases.

--- terre14.rb
+++ terre14.rb
@@ -1,13 +1,19 @@
 # Créez un programme qui détermine si une liste d’entiers est triée ou pas.
+def contains_letter(*args)
+  is_true = false
+  ARGV.select { |e| e.match?(/[a-zA-Z]/) ? (is_true = true) : ()}
+  is_true
+end
 
 def sort_numbers(*args)
   unsorted_numbers = []
-  if ARGV.map{ |e| e.match?(/[a-zA-Z]/)}.any? || ARGV.map{ |e| e.nil? }.any?
-    puts "erreur."
-  else
-    ARGV.map! { |e| e.to_i }.select.with_index { |element, index| (index + 1 == ARGV.length) ? break : (element > ARGV[index + 1]) ? (unsorted_numbers << index) : () }
-  end
-    (unsorted_numbers.length) > 0 ? (puts "Pas triée") : (puts "Triée")
+    ARGV.select.with_index do |element, index|
+       (index + 1 == ARGV.length) ? break : (element > ARGV[index + 1]) ? (unsorted_numbers << index) : ()
+    end
+    (unsorted_numbers.length) > 0 ? ( "Pas triée") : ( "Triée")
 end
 
-sort_numbers
+(puts "error"; exit) if ARGV.size < 2
+(puts "error"; exit) if contains_letter(ARGV)
+
+puts sort_numbers
